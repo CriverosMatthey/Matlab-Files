b6292173a71c1161cb6bfcafbe53f1354b4e4349
CriverosMatthey <cristiandagor@gmail.com>
2019-07-29 12:47:49 +1000

    2nd update

---
+++ PhD_project/Phd/Ross_data/writeModelXml.m
@@ -0,0 +1,15 @@
+%% OPENSIM MODEL STRUCTURE
+
+%% >>>>> Level 1 <<<<<
+S.ATTRIBUTE.Version = '40000';
+
+%% >>>>> Level 2 <<<<<
+S.Model.ATTRIBUTE.name = 'generic_w_cranks_bike';
+
+%% >>>>> Level 3 - Defaults <<<<<
+S.Model.defaults.ControlLinear.ATTRIBUTE.name = 'default';
+S.Model.defaults.CoordinateActuator.ATTRIBUTE.name = 'default';
+S.Model.defaults.PointActuator.ATTRIBUTE.name = 'default';
+S.Model.defaults.TorqueActuator.ATTRIBUTE.name = 'default';
+S.Model.defaults.Millard2012EquilibriumMuscle.ATTRIBUTE.name = 'default';
+S.Model.defaults.CMC_Joint.ATTRIBUTE.name = 'default';
